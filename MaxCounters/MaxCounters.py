bbb7547fb706fad3649382c236b39e6cad60b429
Carmine Choi <carmine.choi@gmail.com>
2023-10-24 21:06:16 -0700

    Task Score: 100% | Correctness Score: 100%

--- MaxCounters/MaxCounters.py
+++ MaxCounters/MaxCounters.py
@@ -5,12 +5,19 @@ def solution(N, A):
     # Implement your solution here
     counters = [0] * N
     maxCount = 0
+    lastMaxCount = 0
 
     for num in A:
         if num >= N + 1:
-            counters = [maxCount] * N
+            lastMaxCount = maxCount
         else:
+            if counters[num-1] < lastMaxCount:
+                counters[num-1] = lastMaxCount
             counters[num-1] += 1
             maxCount = max(maxCount, counters[num-1])
+
+    for i in range(N):
+        if counters[i] < lastMaxCount:
+            counters[i] = lastMaxCount
 
     return counters
